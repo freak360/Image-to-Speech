409b657d05fa88760f5ec7f46e4ed071c7434d7d
Aneeb Ajmal <maneebajmal@gmail.com>
2024-01-04 16:34:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ def img2text(url):
 def story(scenario):
     model_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
     model = HuggingFaceHub(huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN,
-                            repo_id=model_id, model_kwargs={"temperature": 0.8, "max_length": 60})
+                            repo_id=model_id, model_kwargs={"temperature": 0.8, "max_length": 100})
     template = """
     you are a story teller;
-    you can generate a short story based on simple narrative.
+    you can generate a short story based on simple narrative, the story should be no more than 50 words.
     CONTEXT: {scenario}
     STORY: 
     """
